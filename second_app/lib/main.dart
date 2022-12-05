a7afdc994887a53d2b4f0a8ae27cea35fdbca0af
therarErifor <therar.erifor@gmail.com>
2022-12-06 02:12:19 +0500

    upgrade stopwatch

--- second_app/lib/main.dart
+++ second_app/lib/main.dart
@@ -37,27 +37,27 @@ class _MyHomePageState extends State<MyHomePage> {
  initState() {
     super.initState();
      // startTimer();
-    reset();
+    _reset();
   }
-  void reset(){
+  void _reset(){
       setState(() => duration = Duration());
 
   }
 
 
   void addTime() {
-    final addSeconds = 1;
+    final addMilliseconds = 10;
     setState(() {
-      final seconds = duration.inSeconds + addSeconds;
-      duration = Duration(seconds: seconds);
+      final milliseconds = duration.inMilliseconds + addMilliseconds;
+      duration = Duration(milliseconds: milliseconds);
     });
   }
 
-  void startTimer() {
-    timer = Timer.periodic(Duration(seconds: 1), (_) => addTime());
+  void _startTimer() {
+    timer = Timer.periodic(Duration(milliseconds: 10), (_) => addTime());
 
   }
-  void stopTimer(){
+  void _stopTimer(){
     setState(() => timer?.cancel());
   }
 
@@ -72,67 +72,73 @@ class _MyHomePageState extends State<MyHomePage> {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             buildTime(),
-            Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                  children:[
-                    ElevatedButton(
-                      // style: ButtonStyle(
-                      //   foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
-                      // ),
-                      onPressed: () {startTimer();},
-                      child: Text('Start'),
-                    ),
-                      SizedBox(width: 24, height: 100,),
-                    ElevatedButton(
-                      // style: ButtonStyle(
-                      //   foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
-                      // ),
-                      onPressed: () {stopTimer();},
-                      child: Text('Stop'),
-                    ),
-                      SizedBox(width: 24, height: 100,),
-                    ElevatedButton(
-                      // style: ButtonStyle(
-                      //   foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
-                      // ),
-                      onPressed: () {reset();},
-                      child: Text('Reset'),
-                    ),
-            ],
-
-            ),
+            buildButtons(),
           ],
         ),
       );
 
-
-
-        // floatingActionButton: FloatingActionButton(
-        //   onPressed: (){startTimer();},
-        //   tooltip: 'Increment',
-        //   child: const Icon(Icons.timer),
-        // ),
+Widget buildButtons() {
+  final isRunning = timer == null ? false : timer!.isActive;
+  final isCompleted = duration.inMilliseconds == 0;
+ return isRunning || !isCompleted
+  ?Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children:[
+      ElevatedButton(
+        onPressed: () {
+          if (isRunning) {
+            _stopTimer();
+          }else {
+            _startTimer();
+          }
+          },
+        child: Text(isRunning ? 'Stop' : 'Resume'),
+      ),
+      SizedBox(width: 15, height: 100,),
+      ElevatedButton(
+        onPressed: () {_reset();},
+        child: Text('Reset'),
+      ),
+    ],
+ )
+ : Row(mainAxisAlignment: MainAxisAlignment.center,
+     children:[
+       SizedBox(width: 15, height: 100,),
+ ElevatedButton(
+        onPressed: () {_startTimer();},
+        child: Text('Start'),
+      ),
+ ],
+ );
+}
 
   Widget buildTime() {
     String twoDigits(int n) => n.toString().padLeft(2, '0');
+    String Digits(int n) => n.toString().padLeft(3, '0');
     // final hours = twoDigits(duration.inHours);
-    // final minutes = twoDigits(duration.inMinutes.remainder(60));
+    final minutes = twoDigits(duration.inMinutes.remainder(60));
     final seconds = twoDigits(duration.inSeconds.remainder(60));
+    final milliSeconds = Digits(duration.inMilliseconds.remainder(1000));
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         // buildTimeCard(time: hours, header: 'hours'),
         // const SizedBox(width: 8),
-        // buildTimeCard(time: minutes, header: 'minutes'),
-        // const SizedBox(width: 8),
-        buildTimeCard(time: seconds, header: 'seconds'),
+        buildTimeCard(time: minutes),
+        Text(':',
+        style: TextStyle(fontSize: 72, color: Colors.blueAccent)),
+        buildTimeCard(time: seconds),
+        Text('.',
+            style: TextStyle(fontSize: 72, color: Colors.blueAccent)),
+        buildTimeCard(time: milliSeconds.substring(0, 2)),
       ],
     );
 
   }
 
   Widget buildTimeCard({required String time,
-    required String header}) =>
+    }) =>
       Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
@@ -150,9 +156,7 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
           ),
-          const SizedBox(height: 24),
-          Text(header),
-        ],
+  ],
       );
 }
 
